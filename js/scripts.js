ac0a2082c3c308f38005281561d758e6785dcf76
andrew cho and pablo vera <pablovera.pdx@gmail.com>
2016-01-25 14:51:00 -0800

    finalize project with user interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,44 @@
-function Place (city,state) {
+                  // Business Logic
+
+function Place (city,state,timeOfYear,landMark,notes) {
   this.city = city;
   this.state = state;
+  this.timeOfYear = timeOfYear;
+  this.landMark = landMark;
+  this.notes = notes
 }
 
-Place.prototype.cityState = function() {
-  return this.city + ", " + this.state;
-}
+// Place.prototype.cityState = function() {
+//   return this.city + ", " + this.state;
+// }
+
+                // Userinterface
+
+$(document).ready(function() {
+  $("form#new-place").submit(function(){
+    event.preventDefault();
+    var inputtedCity = $('input#new-city').val();
+    var inputtedState = $('input#new-state').val();
+    var inputtedTimeOfYear = $('input#timeOfYear').val();
+    var inputtedLandmark = $('input#landmark').val();
+    var inputtedNotes = $('input#notes').val();
+    var newPlace = new Place (inputtedCity, inputtedState, inputtedTimeOfYear, inputtedLandmark, inputtedNotes);
+
+    $("ul#placesx").append("<li><span class='placesNames'>" + inputtedCity + "</li>");
+
+    inputtedCity = $('input#new-city').val("");
+    inputtedState = $('input#new-state').val("");
+    inputtedTimeOfYear = $('input#timeOfYear').val("");
+    inputtedLandmark = $('input#landmark').val("");
+    inputtedNotes = $('input#notes').val("");
+
+      $(".placesNames").last().click(function() {
+      $("#hide-results").show();
+      $(".city-text").text(newPlace.city);
+      $(".state-text").text(newPlace.state);
+      $(".timeOfYear-text").text(newPlace.timeOfYear);
+      $(".landmark-text").text(newPlace.landMark);
+      $(".notes-text").text(newPlace.notes);
+      });
+    });
+  });
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,14 @@
 describe('Place', function() {
   it("will create function to display places", function(){
-    var testPlace = new Place("Portland","Oregon");
+    var testPlace = new Place("Portland","Oregon","September","statue","It was boring.");
     expect(testPlace.city).to.equal("Portland");
     expect(testPlace.state).to.equal("Oregon");
+    expect(testPlace.timeOfYear).to.equal("September")
+    expect(testPlace.landMark).to.equal("statue")
+    expect(testPlace.notes).to.equal("It was boring.")
   });
-  it("will join our city and state together",function(){
-    var testPlace = new Place("Seattle","Washington");
-    expect(testPlace.cityState()).to.equal("Seattle, Washington");
-  })
+  // it("will join our city and state together",function(){
+  //   var testPlace = new Place("Seattle","Washington");
+  //   expect(testPlace.cityState()).to.equal("Seattle, Washington");
+  // })
 });
